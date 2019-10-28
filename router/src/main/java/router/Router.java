234a5a9349e8236ac647fe0e764f396251b73bbc
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-10-28 23:01:39 +0200

    started building router

--- router/src/main/java/router/Router.java
+++ router/src/main/java/router/Router.java
@@ -1,2 +1,19 @@
 package router;
 
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+
+public class Router implements Runnable {
+	int	port;
+	
+	Router(int port) {
+		this.port = port;
+	}
+
+	@Override
+	public void run() {
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+	}
+
+	
+}
