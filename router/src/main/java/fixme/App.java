554c6691096a42fa445bf8ea38a8a707d47f7c2a
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-10-28 03:06:08 +0200

    added input break loop to main

--- router/src/main/java/fixme/App.java
+++ router/src/main/java/fixme/App.java
@@ -1,17 +1,31 @@
 package fixme;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  */
 public final class App {
-    private App() {
-    }
+	private App() {
+	}
 
-    /**
+	/**
      * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			try {
+				String input = br.readLine();
+				if (input.toUpperCase().equals("EXIT")) {
+					break;
+				}
+			} catch(IOException e) {
+				System.out.println(e.getMessage());
+			}
+		}
     }
 }
