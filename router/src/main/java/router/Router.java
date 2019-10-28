1c619818274c364b4cd452d8a49f2774eea15245
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-10-28 23:28:37 +0200

    implemented Router.run()

--- router/src/main/java/router/Router.java
+++ router/src/main/java/router/Router.java
@@ -1,7 +1,15 @@
 package router;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.EventExecutorGroup;
 
 public class Router implements Runnable {
 	int	port;
@@ -13,7 +21,28 @@ public class Router implements Runnable {
 	@Override
 	public void run() {
 		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup)
+					.channel(NioServerSocketChannel.class)
+					.childHandler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						public void initChannel(SocketChannel ch) throws Exception {
+							ch.pipeline().addLast(
+									new RequestDecoder(), 
+									new ResponseDataEncoder(), 
+									new ProcessingHandler());
+						}
+					}).option(ChannelOption.SO_BACKLOG, 128)
+					.childOption(ChannelOption.SO_KEEPALIVE, true);
+			ChannelFuture f = b.bind(port).sync();
+			f.channel().closeFuture().sync();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
 	}
-
-	
 }
