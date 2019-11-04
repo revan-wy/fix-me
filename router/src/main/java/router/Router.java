d370e8ba464b9dae689a447cba104206dc6773cb
Renzo VAN WYK <revan-wy@c2r6s5.wethinkcode.co.za>
2019-11-04 20:16:09 +0200

    router.run partially implemented

--- router/src/main/java/router/Router.java
+++ router/src/main/java/router/Router.java
@@ -1,15 +1,47 @@
 package router;
 
-public class Router implements Runnable {
+import core.decoders.Decoder;
 
-	public Router(int i) {
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class Router implements Runnable {
+	private int port;
+
+	public Router(int port) {
+		this.port = port;
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
-		// TODO
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup)
+					.channel(NioServerSocketChannel.class)
+					.childHandler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						public void initChannel(SocketChannel ch)
+							throws Exception {
+								ch.pipeline().addLast(
+									new Decoder(),
+									new AcceptConnectionEncoder(),
+									new SellOrBuyEncoder(),
+									new ProcessingHandler()
+							);
+						}
+					}).option(ChannelOption.SO_BACKLOG, 128)
+					.childOption(ChannelOption.SO_KEEPALIVE, true);
+					// TODO complete this block
+		}
+		
+		// TODO complete implementation of run method
 
 	}
 
