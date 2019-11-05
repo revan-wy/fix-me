de6ea001edc5b988c24a9d4f316d1c065e543eca
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-11-05 18:10:40 +0200

    extended MessageToByteEncoder

--- core/src/main/java/core/encoders/SellOrBuyEncoder.java
+++ core/src/main/java/core/encoders/SellOrBuyEncoder.java
@@ -1,6 +1,17 @@
 package core.encoders;
 
-public class SellOrBuyEncoder {
+import core.messages.MessageSellOrBuy;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class SellOrBuyEncoder extends MessageToByteEncoder<MessageSellOrBuy> {
+
+	@Override
+	protected void encode(ChannelHandlerContext ctx, MessageSellOrBuy msg, ByteBuf out) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
 
 	// TODO complete this implementation
 
---
+++ core/src/main/java/core/messages/MessageSellOrBuy.java
@@ -0,0 +1,7 @@
+package core.messages;
+
+public class MessageSellOrBuy {
+
+	// TODO complete this implementation
+
+}
