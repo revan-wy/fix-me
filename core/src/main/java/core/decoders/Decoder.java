2ca2cd51ae5bb24aa24a11158ebb61be186eb716
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-11-05 18:02:05 +0200

    added method stub for extended class

--- core/src/main/java/core/decoders/Decoder.java
+++ core/src/main/java/core/decoders/Decoder.java
@@ -1,6 +1,18 @@
 package core.decoders;
 
-public class Decoder {
+import java.util.List;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ReplayingDecoder;
+
+public class Decoder extends ReplayingDecoder<Object> {
+
+	@Override
+	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
 
 	// TODO implement decoder class
 
