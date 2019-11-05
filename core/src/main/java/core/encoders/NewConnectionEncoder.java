f7d6999a86d6c577e6ee6b6b23f193f9c8b62301
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-11-05 18:06:43 +0200

    extended MessageToByteEncoder

--- core/src/main/java/core/encoders/NewConnectionEncoder.java
+++ core/src/main/java/core/encoders/NewConnectionEncoder.java
@@ -1,6 +1,17 @@
 package core.encoders;
 
-public class NewConnectionEncoder {
+import core.messages.MessageAcceptConnection;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class NewConnectionEncoder extends MessageToByteEncoder<MessageAcceptConnection> {
+
+	@Override
+	protected void encode(ChannelHandlerContext ctx, MessageAcceptConnection msg, ByteBuf out) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
 
 	// TODO complete implementation of this encoder
 
