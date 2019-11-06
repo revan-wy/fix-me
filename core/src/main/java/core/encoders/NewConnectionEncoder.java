ff2e45244b647bfb6eeab39a8a859cd6f9e208da
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-11-06 05:16:43 +0200

    added definition for charset

--- core/src/main/java/core/encoders/NewConnectionEncoder.java
+++ core/src/main/java/core/encoders/NewConnectionEncoder.java
@@ -1,5 +1,7 @@
 package core.encoders;
 
+import java.nio.charset.Charset;
+
 import core.messages.MessageAcceptConnection;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
@@ -10,6 +12,7 @@ public class NewConnectionEncoder extends MessageToByteEncoder<MessageAcceptConn
 	@Override
 	protected void encode(ChannelHandlerContext ctx, MessageAcceptConnection message, ByteBuf out) throws Exception {
 		out.writeInt(message.getTypeLength());
+		Charset charset = Charset.forName("UTF-8");
 		out.writeCharSequence(message.getMessageType(), charset);
 		if (message.getMessageType().equals("MESSAGE_ACCEPT_CONNECTION")) {
 			out.writeInt(message.getId());
