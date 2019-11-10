797a2d43b228e15e6c64e776b9ec2ff97f71936c
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-11-10 20:47:00 +0200

    updated MessageAcceptConnection class

--- core/src/main/java/core/messages/MessageAcceptConnection.java
+++ core/src/main/java/core/messages/MessageAcceptConnection.java
@@ -1,30 +1,24 @@
 package core.messages;
 
+import core.MyChecksum;
+
 public class MessageAcceptConnection extends FixMessage {
 	private int id;
-
-	public void setMessageType(String messageType) {
-
-		// TODO complete this method
-
-	}
-
-	public void setId(int readInt) {
-
-		// TODO complete this method
-
-	}
-
-	public void setChecksum(String string) {
-
-		// TODO complete this method
-
-	}
 
 	public int getId() {
 		return this.id;
 	}
 
-	// TODO complete this implementation
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setNewChecksum(String string) {
+		StringBuilder checksumBuffer = new StringBuilder("");
+		checksumBuffer.append(this.id).append(this.messageType);
+		setChecksum(MyChecksum.myChecksum(checksumBuffer));
+	}
+
+	// search for this // complete this implementation
 
 }
