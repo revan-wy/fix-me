07b9fd72989b3b1a3116a26c8876be894ac92402
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-11-10 14:48:55 +0200

    added String int constructor

--- core/src/main/java/core/messages/FixMessage.java
+++ core/src/main/java/core/messages/FixMessage.java
@@ -1,30 +1,37 @@
 package core.messages;
 
 public class FixMessage {
-	private String	checksum;
 	private int		checksumLength;
-	private String	messageType;
+	private int		marketId;
 	private int		typeLength;
+	private String	checksum;
+	private String	messageType;
 
-	public void setMessageType(String messageType) {
+	public			FixMessage(String messageType, int marketId) {
 		this.messageType = messageType;
+		this.typeLength = messageType.length();
+		this.marketId = marketId;
 	}
 
-	public String getMessageType() {
-		return this.messageType;
-	}
-
-	public int getTypeLength() {
-		return this.typeLength;
-	}
-
-	public int getChecksumLength() {
-		return this.checksumLength;
-	}
-
-	public String getChecksum() {
+	public String	getChecksum() {
 		return this.checksum;
 	}
+
+	public int		getChecksumLength() {
+		return this.checksumLength;
+	}	
+
+	public String	getMessageType() {
+		return this.messageType;
+	}		
+
+	public int		getTypeLength() {
+		return this.typeLength;
+	}		
+
+	public void		setMessageType(String messageType) {
+		this.messageType = messageType;
+	}			
 
 	// TODO complete implementation of this class
 
