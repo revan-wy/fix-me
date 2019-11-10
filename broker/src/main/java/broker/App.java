18e2edf8082951edef616deb2d88de3b308b7b07
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-11-10 23:58:59 +0200

    started work on Client

--- broker/src/main/java/broker/App.java
+++ broker/src/main/java/broker/App.java
@@ -1,17 +1,24 @@
 package broker;
 
+import core.Client;
+
 /**
  * Hello world!
  */
 public final class App {
-	private App() {
-	}
 
-	/**
-	 * Says hello to the world.
-	 * @param args The arguments of the program.
-	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Client client = new Client("Broker");
+		Thread clientThread = new Thread(client);
+		clientThread.start();
+		try {
+			clientThread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+
+		}
+		Client.handleInput(client);
 	}
 }
---
+++ core/src/main/java/core/Client.java
@@ -0,0 +1,19 @@
+package core;
+
+public class Client implements Runnable {
+
+	public Client(String string) {
+
+		// TODO 
+
+	}
+
+	public static void handleInput(Client client) {
+
+		// TODO
+
+	}
+
+	// TODO
+
+}
