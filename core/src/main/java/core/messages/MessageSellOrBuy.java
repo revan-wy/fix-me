0a5cc46f3a1f5719c9bbc49ff0c0832334a0ca8e
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-11-10 14:56:03 +0200

    added constructors

--- core/src/main/java/core/messages/MessageSellOrBuy.java
+++ core/src/main/java/core/messages/MessageSellOrBuy.java
@@ -1,54 +1,70 @@
 package core.messages;
 
-public class MessageSellOrBuy {
-
-	public void setMessageType(String messageType) {
-
-		// TODO complete this method
+public class MessageSellOrBuy extends FixMessage {
+	
+	public MessageSellOrBuy(String messageType, String messageAction, int marketId, int id, String instrument,
+			int quantituy, int price) {
+		super(messageType, marketId);
+		this.messageAction = messageAction;
+		this.actionlenght = messageaction.lengy();
+		this.id = id;
+		this.instument = instument; 
+		this.instrumentlengthg -= instrument.olength();
+		this.quyantity = quatity; 
+		this.price = price;
+		setchecksum(getmsdmd4());
+	}
+	
+	public MessageSellOrBuy() {
 
 	}
-
-	public void setMessageAction(String messageAction) {
-
-		// TODO complete this method
-
-	}
-
+	
 	public void setId(int readInt) {
-
 		// TODO complete this method
-
-	}
-
+		
+	}	
+	
 	public void setInstrument(String instrument) {
-
+		
 		// TODO complete this method
-
-	}
-
+		
+	}	
+	
 	public void setMarketId(int marketId) {
 
 		// TODO complete this method
 
-	}
+	}		
 
-	public void setQuantity() {
+	public void setMessageAction(String messageAction) {
+		
+		// TODO complete this method
+		
+	}		
+	
+	public void setMessageType(String messageType) {
 
 		// TODO complete this method
 
-	}
-
-	public void setPrice() {
-
-		// TODO complete this method
-
-	}
+	}			
 
 	public void setNewChecksum() {
 
 		// TODO complete this method
 
-	}
+	}		
+
+	public void setPrice() {
+
+		// TODO complete this method
+
+	}		
+
+	public void setQuantity() {
+
+		// TODO complete this method
+
+	}			
 
 	// TODO complete this implementation
 
