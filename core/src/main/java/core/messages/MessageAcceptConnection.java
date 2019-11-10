02e98c20df7723cab989a377ca5ac82d66307d83
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-11-10 23:47:34 +0200

    complete MessageAcceptConnection

--- core/src/main/java/core/messages/MessageAcceptConnection.java
+++ core/src/main/java/core/messages/MessageAcceptConnection.java
@@ -5,20 +5,42 @@ import core.MyChecksum;
 public class MessageAcceptConnection extends FixMessage {
 	private int id;
 
-	public int getId() {
+	public			MessageAcceptConnection(String messageType, int marketId, int id) {
+		super(messageType, marketId);
+		this.id = id;
+		setChecksum(createMyChecksum());
+	}
+	
+	public			MessageAcceptConnection() {
+
+	}
+	
+	public String	createMyChecksum() {
+		StringBuilder checksumBuffer = new StringBuilder("");
+		checksumBuffer.append(marketId).append(messageType).append(id);
+		return MyChecksum.myChecksum(checksumBuffer);
+	}
+
+	public int		getId() {
 		return this.id;
 	}
 
-	public void setId(int id) {
+	public void		setId(int id) {
 		this.id = id;
 	}
 
-	public void setNewChecksum() {
+	public void		setNewChecksum() {
 		StringBuilder checksumBuffer = new StringBuilder("");
 		checksumBuffer.append(this.id).append(this.messageType);
 		setChecksum(MyChecksum.myChecksum(checksumBuffer));
 	}
 
-	// search for this // complete this implementation
-
+	@Override
+	public String	toString() {
+		return "messageAcceptConnection { " +
+			"ID = " + this.getId() +
+			" | MESSAGE_TYPE = " + getMessageType() +
+			" | CHECKSUM = " + getChecksum() +
+			"}";
+	}
 }
