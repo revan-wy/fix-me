eb36db5c6d57bef5d7a86f21eb38e162465dc411
Renzo VAN WYK <revan-wy@c3r2s1.wethinkcode.co.za>
2019-11-18 17:27:20 +0200

    formatted Broker

--- broker/src/main/java/broker/Broker.java
+++ broker/src/main/java/broker/Broker.java
@@ -3,16 +3,14 @@ package broker;
 import core.Client;
 
 public class Broker {
-    public static void main(String[] args) {
-        Client client = new Client(Client.Type.BROKER);
-        Thread clientThread = new Thread(client);
-        clientThread.start();
-        try {
-            clientThread.join();
-        } catch (InterruptedException e) {
-            System.out.println(e.getMessage());
-        }
+	public static void main(String[] args) {
+		Client client = new Client(Client.Type.BROKER);
+		Thread clientThread = new Thread(client);
+		clientThread.start();
+		try {
+			clientThread.join();
+		} catch (InterruptedException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
-
-// TODO format
