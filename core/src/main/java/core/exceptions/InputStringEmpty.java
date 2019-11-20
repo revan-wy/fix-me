4dbfcdd12a5d8c99eb361ab7009f1dfdb25f506f
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-11-20 22:25:32 +0200

    streamlined InputStringEmpty

--- core/src/main/java/core/exceptions/InputStringEmpty.java
+++ core/src/main/java/core/exceptions/InputStringEmpty.java
@@ -1,11 +1,9 @@
 package core.exceptions;
 
 public class InputStringEmpty extends Exception {
-	private static final long serialVersionUID = 9128473251675469018L;
+	private static final long serialVersionUID = -6873250619293743097L;
 
 	public InputStringEmpty() {
 		super("Input string is empty.");
 	}
 }
-
-// TODO  format
