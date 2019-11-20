7a855d07f5759dce50743c4ee0c0d5e758f5087f
revan-wy <revan-wy@student.wethinkcode.co.za>
2019-11-20 08:25:01 +0200

    streamlined client

--- core/src/main/java/core/Client.java
+++ core/src/main/java/core/Client.java
@@ -33,15 +33,144 @@ public class Client implements Runnable {
 	private int port;
 	private String host = "localhost";
 
-	public enum Type {
-		BROKER, MARKET
-	}
-
 	public Client(Client.Type clientType) {
 		this.clientType = clientType;
 		this.port = 5000;
 		if (clientType == Client.Type.MARKET)
-			port = 5001;
+			this.port = 5001;
+	}
+
+	class ClientHandler extends ChannelInboundHandlerAdapter {
+		private void announceNewConnection(Object message) {
+			ConnectionRequest request = (ConnectionRequest) message;
+			clientID = request.getId();
+			System.out.println("Client connected to router with ID: " + clientID);
+		}
+
+		private void brokerWriteHandler(ChannelHandlerContext context, String string) throws Exception {
+			String[] split = string.split("\\s+");
+			if (split.length != 5)
+				throw new BrokerInputError();
+			BuyOrSellOrder message;
+			if (split[0].toLowerCase().equals("sell")) {
+				message = new BuyOrSellOrder(Message.Type.SELL.toString(), "", verifyId(split[1]), clientID, split[2],
+						Integer.valueOf(split[3]), Integer.valueOf(split[4]));
+			} else if (split[0].toLowerCase().equals("buy")) {
+				message = new BuyOrSellOrder(Message.Type.BUY.toString(), "", verifyId(split[1]), clientID, split[2],
+						Integer.valueOf(split[3]), Integer.valueOf(split[4]));
+			} else {
+				throw new BrokerInputError();
+			}
+			message.updateChecksum();
+			context.writeAndFlush(message);
+			System.out.println("Sending " + message.getMessageType() + " order to router.");
+		}
+
+		@Override
+		public void channelActive(ChannelHandlerContext context) throws Exception {
+			System.out.println("Connection request sent to router.");
+			ConnectionRequest message = new ConnectionRequest(Message.Type.CONNECTION_REQUEST.toString());
+			context.writeAndFlush(message);
+		}
+
+		@Override
+		public void channelRead(ChannelHandlerContext context, Object msg) {
+			FixMessage message = (FixMessage) msg;
+			if (message.getMessageType().equals(Message.Type.CONNECTION_REQUEST.toString())) {
+				announceNewConnection(msg);
+			} else if (messageIsBuyOrSell(message)) {
+				BuyOrSellOrder request = (BuyOrSellOrder) msg;
+				try {
+					if (!request.createMyChecksum().equals(request.getChecksum()))
+						throw new ChecksumIsInvalid();
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+					return;
+				}
+				if (messageHasBeenActioned(request))
+					return;
+				if (message.getMessageType().equals(Message.Type.SELL.toString()))
+					marketSellOrderHandler(context, request);
+				else
+					marketBuyOrderHandler(context, request);
+			}
+		}
+
+		@Override
+		public void channelReadComplete(ChannelHandlerContext context) {
+			if (clientType == Client.Type.BROKER)
+				channelWrite(context);
+		}
+
+		private void channelWrite(ChannelHandlerContext context) {
+			try {
+				String input = getBrokerInput();
+				if (input.length() == 0)
+					throw new InputStringEmpty();
+				else if (input.toLowerCase().equals("exit"))
+					shutdown();
+				else if (clientType == Client.Type.BROKER)
+					brokerWriteHandler(context, input);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				channelWrite(context);
+			}
+		}
+
+		private String getBrokerInput() throws Exception {
+			System.out.println(
+					"Please create a new order. Format: \n[sell || buy] [market id] [instrument] [quantity] [price]");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			return br.readLine();
+		}
+
+		private boolean messageHasBeenActioned(BuyOrSellOrder message) {
+			if (message.getMessageAction().equals(Message.Action.EXECUTED.toString())
+					|| message.getMessageAction().equals(Message.Action.REJECTED.toString())) {
+				System.out
+						.println("Response to " + message.getMessageType() + " order : " + message.getMessageAction());
+				return true;
+			}
+			return false;
+		}
+
+		private boolean messageIsBuyOrSell(FixMessage message) {
+			return message.getMessageType().equals(Message.Type.BUY.toString())
+					|| message.getMessageType().equals(Message.Type.SELL.toString());
+		}
+
+		private void marketBuyOrderHandler(ChannelHandlerContext context, BuyOrSellOrder message) {
+			Random random = new Random();
+			if (random.nextBoolean()) {
+				System.out.println("Buy order rejected.");
+				message.setMessageAction(Message.Action.REJECTED.toString());
+			} else {
+				System.out.println("Buy order successfully executed.");
+				message.setMessageAction(Message.Action.EXECUTED.toString());
+			}
+			message.updateChecksum();
+			context.writeAndFlush(message);
+		}
+
+		private void marketSellOrderHandler(ChannelHandlerContext context, BuyOrSellOrder message) {
+			Random random = new Random();
+			if (random.nextBoolean()) {
+				System.out.println("Sell order successfully executed.");
+				message.setMessageAction(Message.Action.EXECUTED.toString());
+			} else {
+				System.out.println("Sell order rejected.");
+				message.setMessageAction(Message.Action.REJECTED.toString());
+			}
+			message.updateChecksum();
+			context.writeAndFlush(message);
+		}
+
+		private int verifyId(String id) throws Exception {
+			if (id.length() != 6)
+				throw new BrokerInputError();
+			return Integer.valueOf(id);
+		}
+
 	}
 
 	public static void inputHandler(Client client) {
@@ -85,136 +214,8 @@ public class Client implements Runnable {
 		workerGroup.shutdownGracefully();
 	}
 
-	class ClientHandler extends ChannelInboundHandlerAdapter {
-		@Override
-		public void channelActive(ChannelHandlerContext context) throws Exception {
-			System.out.println("Connection request sent to router.");
-			ConnectionRequest message = new ConnectionRequest(Message.Type.CONNECTION_REQUEST.toString());
-			context.writeAndFlush(message);
-		}
-
-		@Override
-		public void channelRead(ChannelHandlerContext context, Object msg) {
-			FixMessage message = (FixMessage) msg;
-			if (message.getMessageType().equals(Message.Type.CONNECTION_REQUEST.toString())) {
-				announceNewConnection(msg);
-			} else if (messageIsBuyOrSell(message)) {
-				BuyOrSellOrder request = (BuyOrSellOrder) msg;
-				try {
-					if (!request.createMyChecksum().equals(request.getChecksum()))
-						throw new ChecksumIsInvalid();
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-					return;
-				}
-				if (messageHasBeenActioned(request))
-					return;
-				if (message.getMessageType().equals(Message.Type.SELL.toString()))
-				marketSellOrderHandler(context, request);
-				else
-				marketBuyOrderHandler(context, request);
-			}
-		}
-
-		private boolean messageIsBuyOrSell(FixMessage message) {
-			return message.getMessageType().equals(Message.Type.BUY.toString())
-					|| message.getMessageType().equals(Message.Type.SELL.toString());
-		}
-
-		private void announceNewConnection(Object message) {
-			ConnectionRequest request = (ConnectionRequest) message;
-			clientID = request.getId();
-			System.out.println("Client connected to router with ID: " + clientID);
-		}
-
-		private boolean messageHasBeenActioned(BuyOrSellOrder message) {
-			if (message.getMessageAction().equals(Message.Action.EXECUTED.toString())
-					|| message.getMessageAction().equals(Message.Action.REJECTED.toString())) {
-				System.out.println("Response to " + message.getMessageType() + " order : " + message.getMessageAction());
-				return true;
-			}
-			return false;
-		}
-
-		private void marketSellOrderHandler(ChannelHandlerContext context, BuyOrSellOrder message) {
-			Random random = new Random();
-			if (random.nextBoolean()) {
-				System.out.println("Sell order successfully executed.");
-				message.setMessageAction(Message.Action.EXECUTED.toString());
-			} else {
-				System.out.println("Sell order rejected.");
-				message.setMessageAction(Message.Action.REJECTED.toString());
-			}
-			message.updateChecksum();
-			context.writeAndFlush(message);
-		}
-
-		private void marketBuyOrderHandler(ChannelHandlerContext context, BuyOrSellOrder message) {
-			Random random = new Random();
-			if (random.nextBoolean()) {
-				System.out.println("Buy order rejected.");
-				message.setMessageAction(Message.Action.REJECTED.toString());
-			} else {
-				System.out.println("Buy order successfully executed.");
-				message.setMessageAction(Message.Action.EXECUTED.toString());
-			}
-			message.updateChecksum();
-			context.writeAndFlush(message);
-		}
-
-		private void channelWrite(ChannelHandlerContext context) {
-			try {
-				String input = getBrokerInput();
-				if (input.length() == 0)
-					throw new InputStringEmpty();
-				else if (input.toLowerCase().equals("exit"))
-					shutdown();
-				else if (clientType == Client.Type.BROKER)
-				brokerWriteHandler(context, input);
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-				channelWrite(context);
-			}
-		}
-
-		private void brokerWriteHandler(ChannelHandlerContext context, String string) throws Exception {
-			String[] split = string.split("\\s+");
-			if (split.length != 5)
-				throw new BrokerInputError();
-			BuyOrSellOrder message;
-			if (split[0].toLowerCase().equals("sell")) {
-				message = new BuyOrSellOrder(Message.Type.SELL.toString(), "", verifyId(split[1]), clientID, split[2], Integer.valueOf(split[3]),
-				Integer.valueOf(split[4]));
-			} else if (split[0].toLowerCase().equals("buy")) {
-				message = new BuyOrSellOrder(Message.Type.BUY.toString(), "", verifyId(split[1]), clientID, split[2], Integer.valueOf(split[3]),
-				Integer.valueOf(split[4]));
-			} else{
-				throw new BrokerInputError();}
-				message.updateChecksum();
-			context.writeAndFlush(message);
-			System.out.println("Sending " + message.getMessageType() + " order to router.");
-		}
-
-		private int verifyId(String id) throws Exception {
-			if (id.length() != 6)
-			throw new BrokerInputError();
-			return Integer.valueOf(id);
-		}
-
-		@Override
-		public void channelReadComplete(ChannelHandlerContext context) {
-			if (clientType == Client.Type.BROKER)
-				channelWrite(context);
-		}
-
-		private String getBrokerInput() throws Exception {
-			System.out.println(
-					"Enter request message of type: [sell || buy] [market id] [instrument] [quantity] [price]");
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			return br.readLine();
-		}
+	public enum Type {
+		BROKER, MARKET
 	}
-}
 
-// TODO alphabetise
-// TODO format
+}
