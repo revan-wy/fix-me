e39a1c812f6f3020f7d3f3d1c4b88149174ca41d
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-11-20 22:08:53 +0200

    streamlined ChecksumIsInvalid

--- core/src/main/java/core/exceptions/ChecksumIsInvalid.java
+++ core/src/main/java/core/exceptions/ChecksumIsInvalid.java
@@ -1,11 +1,9 @@
 package core.exceptions;
 
 public class ChecksumIsInvalid extends Exception {
-	private static final long serialVersionUID = -3408599660178491251L;
+	private static final long serialVersionUID = 7029294679968048090L;
 
 	public ChecksumIsInvalid() {
 		super("Checksum is invalid.");
 	}
 }
-
-// TODO format
